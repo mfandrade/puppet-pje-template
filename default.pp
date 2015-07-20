aa6cfafed19f13dfa6bb0cb100351ac36b05cd40
Marcelo F Andrade <mfandrade@gmail.com>
2015-07-20 12:43:04 -0300

    Volta a usar o módulo PJE

--- default.pp
+++ default.pp
@@ -1,16 +1,16 @@
 #notify { 'BOM DIA, FLOR DO DIA!': }
 
 node /^pje8-jb-(int|ext)-[a-z].trt8.net$/ {
-  #include pje
+  include pje
 
   notify { "DEPLOYANDO em uma máquina $1": }
 
-  #pje::profile { 'pje1hx':
+  pje::profile { 'pje1hx':
     #binding_ipaddr => '0.0.0.0',
-  #  binding_ports  => 'ports-01',
-  #  jmxremote_port => '9001',
+    binding_ports  => 'ports-01',
+    jmxremote_port => '9001',
     #quartz         => false
-  #}
+  }
 
   #pje::profile { 'pje2hx':
   #  binding_ports  => 'ports-02',
