f44d5428c64788df8fb4e281f9c9edf2f3f26a56
Marcelo F Andrade <mfandrade@gmail.com>
2015-07-30 17:21:50 -0300

    Deixa descomentado o segundo grau

--- default.pp
+++ default.pp
@@ -8,11 +8,11 @@ node /^pje8-jb-((int|ext)[a-z]).trt8.net$/ {
     ds_databasename => 'pje_1grau_bugfix',
   }
 
-#  pje::profile { "${1}2":
-#    version         => '1.5.2.3',
-#    env             => 'bugfix',
-#    binding_to      => '0.0.0.0',
-#    jmxremote_port  => 9002,
-#    ds_databasename => 'pje_2grau_bugfix',
-#  }
+  pje::profile { "${1}2":
+    version         => '1.5.2.3',
+    env             => 'bugfix',
+    binding_to      => '0.0.0.0',
+    jmxremote_port  => 9002,
+    ds_databasename => 'pje_2grau_bugfix',
+  }
 }
