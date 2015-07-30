e05a7a457273151ef8cf8b8cb2eb3d43553c8ce7
Marcelo F Andrade <mfandrade@gmail.com>
2015-07-30 14:17:52 -0300

    Identação e alguns ajustes em nomes de variáveis

--- modules/pje/templates/pje-xgrau-default.sh
+++ modules/pje/templates/pje-xgrau-default.sh
@@ -37,28 +37,6 @@ fi
 JBOSS_JNP_PORT="$JNP_PORT"
 JBOSS_LOG_LEVEL=${JBOSS_LOG_LEVEL:-"ERROR"}
 
-JBOSS_LOG_DIR=${JBOSS_LOG_DIR:-"$JBOSS_HOME/server/$JBOSS_PROFILE/log"}
-JBOSS_USER=${JBOSS_USER:-"jboss"}
-CLEAR_WORK_TMP=${CLEAR_WORK_TMP:-"Y"}
-
-# Clustering Configs
-# Fill these only when using profiling supporting clustering. Otherise they'll be ignored by the script
-#    -g, --partition=<name>        HA Partition name (default=DefaultDomain)
-#    -m, --mcast_port=<ip>         UDP multicast port; only used by JGroups
-#    -u, --udp=<ip>                UDP multicast address
-#    -Djboss.default.jgroups.stack=udp|udp-async|udp-sync|tcp|tcp-sync
-  CLUSTER_PARTITION=${CLUSTER_PARTITION:-"DefaultPartition"}
-  CLUSTER_JGROUPS_STACK=${CLUSTER_JGROUPS_STACK:-"udp"}
-  CLUSTER_UDP_MCAST_ADDR=${CLUSTER_UDP_MCAST_ADDR:-"228.11.11.11"}
-  CLUSTER_UDP_MCAST_PORT=${CLUSTER_UDP_MCAST_PORT:-"55225"}
-
-# JMX Credentials
-  JMX_USERS_PROPERTIES_FILE="$JBOSS_HOME/server/$JBOSS_PROFILE/conf/props/jmx-console-users.properties"
-  JMX_CREDENTIALS=$(grep -Eo '^[[:alnum:]]+=[[:print:]]+' $JMX_USERS_PROPERTIES_FILE)
-  JMX_USER=$(cut -d= -f1 <<<$JMX_CREDENTIALS)
-  JMX_PASS=$(cut -d= -f2 <<<$JMX_CREDENTIALS)
-  JBOSS_ADMIN_USER=${JMX_USER:-"admin"}
-  JBOSS_ADMIN_PASS=${JMX_PASS:-"admin"}
 
 # make sure java is in your path
 JAVABIN=$(which java 2>/dev/null)
@@ -71,192 +49,189 @@ if [[ "$JAVABIN" = "" ]]; then
     fi
 fi
 
-# define the classpath for the shutdown class
-  JBOSSCP=${JBOSSCP:-"$JBOSS_HOME/bin/shutdown.jar:$JBOSS_HOME/client/jnet.jar"}
+if [ ! -d "$JBOSS_HOME" ]; then
+    echo "JBOSS_HOME does not exist as a valid directory: $JBOSS_HOME"
+    exit 1
+fi
+
+JBOSS_LOG_DIR=${JBOSS_LOG_DIR:-"$JBOSS_HOME/server/$JBOSS_PROFILE/log"}
+JBOSS_USER=${JBOSS_USER:-"jboss"}
+CLEAR_WORK_TMP=${CLEAR_WORK_TMP:-"Y"}
+
+# Clustering Configs
+# Fill these only when using profiling supporting clustering. Otherise they'll be ignored by the script
+#    -g, --partition=<name>        HA Partition name (default=DefaultDomain)
+#    -m, --mcast_port=<ip>         UDP multicast port; only used by JGroups
+#    -u, --udp=<ip>                UDP multicast address
+#    -Djboss.default.jgroups.stack=udp|udp-async|udp-sync|tcp|tcp-sync
+CLUSTER_PARTITION=${CLUSTER_PARTITION:-"DefaultPartition"}
+CLUSTER_JGROUPS_STACK=${CLUSTER_JGROUPS_STACK:-"udp"}
+CLUSTER_UDP_MCAST_ADDR=${CLUSTER_UDP_MCAST_ADDR:-"228.11.11.11"}
+CLUSTER_UDP_MCAST_PORT=${CLUSTER_UDP_MCAST_PORT:-"55225"}
+
+# JMX Credentials
+JMX_USERS_PROPERTIES_FILE="$JBOSS_HOME/server/$JBOSS_PROFILE/conf/props/jmx-console-users.properties"
+JMX_CREDENTIALS=$(grep -Eo '^[[:alnum:]]+=[[:print:]]+' $JMX_USERS_PROPERTIES_FILE)
+JMX_USER=$(cut -d= -f1 <<<$JMX_CREDENTIALS)
+JMX_PASS=$(cut -d= -f2 <<<$JMX_CREDENTIALS)
+JBOSS_ADMIN_USER=${JMX_USER:-"admin"}
+JBOSS_ADMIN_PASS=${JMX_PASS:-"admin"}
 
 # define the script to use to start jboss
 # test if the profile has cluster support
 if [ -e $JBOSS_HOME/server/$JBOSS_PROFILE/deploy/cluster ]; then
-   JBOSSSH="$JBOSS_HOME/bin/run.sh -c $JBOSS_PROFILE -b $JBOSS_BINDING_IPADDR -g $CLUSTER_PARTITION -Djboss.default.jgroups.stack=$CLUSTER_JGROUPS_STACK"
-
-   if [[ "$CLUSTER_JGROUPS_STACK" =~ udp* ]]; then
-     JBOSSSH="$JBOSSSH -u $CLUSTER_UDP_MCAST_ADDR -m $CLUSTER_UDP_MCAST_PORT "
-   fi
-
+    RUNSH="$JBOSS_HOME/bin/run.sh -c $JBOSS_PROFILE -b $JBOSS_BINDING_IPADDR -g $CLUSTER_PARTITION -Djboss.default.jgroups.stack=$CLUSTER_JGROUPS_STACK"
+    if [[ "$CLUSTER_JGROUPS_STACK" =~ udp* ]]; then
+        RUNSH="$RUNSH -u $CLUSTER_UDP_MCAST_ADDR -m $CLUSTER_UDP_MCAST_PORT"
+    fi
 else
-  JBOSSSH=${JBOSSSH:-"$JBOSS_HOME/bin/run.sh -c $JBOSS_PROFILE -b $JBOSS_BINDING_IPADDR "}
+    RUNSH="$JBOSS_HOME/bin/run.sh -c $JBOSS_PROFILE -b $JBOSS_BINDING_IPADDR"
 fi
 
 # get the current user
-  CURRENT_USER=`whoami`
+CURRENT_USER=`whoami`
 
 if [ "$JBOSS_USER" = "RUNASIS" -o "$JBOSS_USER" = "$CURRENT_USER"  ]; then
-  SUBIT=""
+    SU_USER=""
 else
-  SUBIT="su - $JBOSS_USER -c "
-  SUBIT="su -l $JBOSS_USER -c "
+    SU_USER="su -l $JBOSS_USER -c"
 fi
 
-if [ -n "$JBOSS_CONSOLE" -a ! -d "$JBOSS_CONSOLE" ]; then
-  # ensure the file exists
-  touch $JBOSS_CONSOLE
-  if [ ! -z "$SUBIT" ]; then
-    chown $JBOSS_USER $JBOSS_CONSOLE
-  fi 
-fi
-
-if [ -n "$JBOSS_CONSOLE" -a ! -f "$JBOSS_CONSOLE" ]; then
-  #echo "WARNING: location for saving console log invalid: $JBOSS_CONSOLE"
-  #echo "WARNING: ignoring it and using /dev/null"
-  JBOSS_CONSOLE="/dev/null"
-fi
-
-#define what will be done with the console log
+# define what will be done with the console log
 JBOSS_CONSOLE=${JBOSS_CONSOLE:-"/dev/null"}
+if [ ! -z "$SU_USER" ]; then
+    chown $JBOSS_USER $JBOSS_CONSOLE
+fi
 
-JBOSS_JVM_PROP="-Djboss.service.binding.set=$JBOSS_BINDING_PORTS \
+JBOSS_JVM_PROPS="-Djboss.service.binding.set=$JBOSS_BINDING_PORTS \
     -Djboss.server.log.threshold=$JBOSS_LOG_LEVEL \
     -Djboss.server.log.dir=$JBOSS_LOG_DIR"
+JBOSS_CMD_START="$RUNSH $JBOSS_JVM_PROPS"
 
-JBOSS_CMD_START="$JBOSSSH $JBOSS_JVM_PROP"
-
+JBOSSCP=${JBOSSCP:-"$JBOSS_HOME/bin/shutdown.jar:$JBOSS_HOME/client/jnet.jar"} # used for shutdown
 JBOSS_CMD_STOP="$JAVABIN -classpath $JBOSSCP org.jboss.Shutdown --shutdown \
     -s jnp://$JBOSS_BINDING_IPADDR:$JBOSS_JNP_PORT \
     -u $JBOSS_ADMIN_USER -p $JBOSS_ADMIN_PASS"
 
-if [ ! -d "$JBOSS_HOME" ]; then
-   echo "JBOSS_HOME does not exist as a valid directory: $JBOSS_HOME"
-   exit 1
-fi
-
-function twiddleInfo()
+twiddleInfo()
 {
-   # use twiddle to get some server status 
-   TWIDDLE_CMD="$JBOSS_HOME/bin/twiddle.sh -s jnp://$JBOSS_BINDING_IPADDR:$JBOSS_JNP_PORT -u $JBOSS_ADMIN_USER -p $JBOSS_ADMIN_PASS"
-   TWIDDLE_CMD_GET="$TWIDDLE_CMD get"
-   TWIDDLE_CMD_QRY="$TWIDDLE_CMD query"
-   TWIDDLE_CMD_IVK="$TWIDDLE_CMD invoke"
- 
-   SERVER_MBEAN="jboss.system:type=Server"
-   SERVER_INFO_MBEAN="jboss.system:type=ServerInfo"
-   JBOSS_WEB_THREADPOOL_MBEAN="jboss.web:type=ThreadPool,name=http-0.0.0.0-8009"
-   JBOSS_WEB_GLOBAL_REQUEST_PROCESSOR_MBEAN="jboss.web:type=GlobalRequestProcessor,name=http-0.0.0.0-8009"
-   JBOSS_WEB_DEPLOYMENTS_MBEAN="jboss.web.deployment:*"
-   JBOSS_JCA_MBEAN="jboss.jca:*"
-   WEB_APP_MBEAN="jboss.web:type=Manager,path="
+    # use twiddle to get some server status 
+    TWIDDLE_CMD="$JBOSS_HOME/bin/twiddle.sh -s jnp://$JBOSS_BINDING_IPADDR:$JBOSS_JNP_PORT -u $JBOSS_ADMIN_USER -p $JBOSS_ADMIN_PASS"
+    TWIDDLE_CMD_GET="$TWIDDLE_CMD get"
+    TWIDDLE_CMD_QRY="$TWIDDLE_CMD query"
+    TWIDDLE_CMD_IVK="$TWIDDLE_CMD invoke"
 
-   #Server Info
-   echo "      |--- $($TWIDDLE_CMD_GET $SERVER_MBEAN VersionName)"
-   echo "      |--- $($TWIDDLE_CMD_GET $SERVER_MBEAN VersionNumber)"
-   echo "      |--- $($TWIDDLE_CMD_GET $SERVER_MBEAN StartDate)"
-   echo "      |--- $($TWIDDLE_CMD_GET $SERVER_INFO_MBEAN JavaVersion)"
-   echo "          JVM Flags"
+    SERVER_MBEAN="jboss.system:type=Server"
+    SERVER_INFO_MBEAN="jboss.system:type=ServerInfo"
+    JBOSS_WEB_THREADPOOL_MBEAN="jboss.web:type=ThreadPool,name=http-0.0.0.0-8009"
+    JBOSS_WEB_GLOBAL_REQUEST_PROCESSOR_MBEAN="jboss.web:type=GlobalRequestProcessor,name=http-0.0.0.0-8009"
+    JBOSS_WEB_DEPLOYMENTS_MBEAN="jboss.web.deployment:*"
+    JBOSS_JCA_MBEAN="jboss.jca:*"
+    WEB_APP_MBEAN="jboss.web:type=Manager,path="
 
-   if [ -e "$JAVA_HOME/bin/jinfo" ]; then
-      $JAVA_HOME/bin/jinfo -flags $PID 2>&1 | grep "run.sh" | tr ' ' '\n'
-   fi
+    #Server Info
+    echo "      |--- $($TWIDDLE_CMD_GET $SERVER_MBEAN VersionName)"
+    echo "      |--- $($TWIDDLE_CMD_GET $SERVER_MBEAN VersionNumber)"
+    echo "      |--- $($TWIDDLE_CMD_GET $SERVER_MBEAN StartDate)"
+    echo "      |--- $($TWIDDLE_CMD_GET $SERVER_INFO_MBEAN JavaVersion)"
+    echo "          JVM Flags"
 
-   echo " "
-   echo "      |--- $($TWIDDLE_CMD_GET $SERVER_INFO_MBEAN ActiveThreadCount)"
+    if [ -e "$JAVA_HOME/bin/jinfo" ]; then
+        $JAVA_HOME/bin/jinfo -flags $PID 2>&1 | grep "run.sh" | tr ' ' '\n'
+    fi
 
-   MaxMemInBytes=`$TWIDDLE_CMD_GET $SERVER_INFO_MBEAN MaxMemory | cut -d '=' -f 2`
-   MaxMemInMB=`echo "($MaxMemInBytes/1024/1024)" | bc`
-   echo "      |--- MaxMemory = $MaxMemInMB MB"
+    echo " "
+    echo "      |--- $($TWIDDLE_CMD_GET $SERVER_INFO_MBEAN ActiveThreadCount)"
 
-   FreeMemInBytes=`$TWIDDLE_CMD_GET $SERVER_INFO_MBEAN FreeMemory | cut -d '=' -f 2`
-   FreeMemInMB=`echo "($FreeMemInBytes/1024/1024)" | bc`
-   echo "      |--- FreeMemory = $FreeMemInMB MB"
+    MaxMemInBytes=`$TWIDDLE_CMD_GET $SERVER_INFO_MBEAN MaxMemory | cut -d '=' -f 2`
+    MaxMemInMB=`echo "($MaxMemInBytes/1024/1024)" | bc`
+    echo "      |--- MaxMemory = $MaxMemInMB MB"
 
-   #HTTP ThreadPool
-   echo " "
-   echo "   JBossWEB "
-   echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_THREADPOOL_MBEAN maxThreads)" | egrep -v "ERROR|at |Exception"
-   echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_THREADPOOL_MBEAN currentThreadCount)" | egrep -v "ERROR|at |Exception"
-   echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_THREADPOOL_MBEAN currentThreadsBusy)" | egrep -v "ERROR|at |Exception"
+    FreeMemInBytes=`$TWIDDLE_CMD_GET $SERVER_INFO_MBEAN FreeMemory | cut -d '=' -f 2`
+    FreeMemInMB=`echo "($FreeMemInBytes/1024/1024)" | bc`
+    echo "      |--- FreeMemory = $FreeMemInMB MB"
 
-   #HTTP GlobalRequestProcessor
-   echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_GLOBAL_REQUEST_PROCESSOR_MBEAN requestCount)" | egrep -v "ERROR|at |Exception"
+    #HTTP ThreadPool
+    echo " "
+    echo "   JBossWEB "
+    echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_THREADPOOL_MBEAN maxThreads)" | egrep -v "ERROR|at |Exception"
+    echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_THREADPOOL_MBEAN currentThreadCount)" | egrep -v "ERROR|at |Exception"
+    echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_THREADPOOL_MBEAN currentThreadsBusy)" | egrep -v "ERROR|at |Exception"
 
-   #WebApps 
-   echo "      |--- Webapps "
-       
-   for WEB_APP in `$TWIDDLE_CMD_QRY $JBOSS_WEB_DEPLOYMENTS_MBEAN | cut -d '=' -f 2 | grep -v 'ROOT'`
-   do
-      echo "      |------ $WEB_APP"
-      echo "      |---------> $($TWIDDLE_CMD_GET "${WEB_APP_MBEAN}${WEB_APP},host=localhost" activeSessions)" | egrep -v "ERROR|at |Exception"
-   done
+    #HTTP GlobalRequestProcessor
+    echo "      |--- $($TWIDDLE_CMD_GET $JBOSS_WEB_GLOBAL_REQUEST_PROCESSOR_MBEAN requestCount)" | egrep -v "ERROR|at |Exception"
 
-   echo " "
+    #WebApps 
+    echo "      |--- Webapps "
 
-   #DataSources
-   echo "   Data Sources "
-       
-   for DS in `$TWIDDLE_CMD_QRY $JBOSS_JCA_MBEAN | grep ManagedConnectionPool`
-   do
-      echo "      |--- `echo $DS | cut -d ',' -f 2`"
-      echo "      |---------> $($TWIDDLE_CMD_GET $DS MaxSize)" | egrep -v "ERROR|at |Exception"
-      echo "      |---------> $($TWIDDLE_CMD_GET $DS AvailableConnectionCount)" | egrep -v "ERROR|at |Exception"
-      echo "      |---------> $($TWIDDLE_CMD_GET $DS InUseConnectionCount)" | egrep -v "ERROR|at |Exception"
-      echo "      |---------> Test Connection: $($TWIDDLE_CMD_IVK $DS testConnection)" | egrep -v "ERROR|at |Exception"
-   done
+    for WEB_APP in `$TWIDDLE_CMD_QRY $JBOSS_WEB_DEPLOYMENTS_MBEAN | cut -d '=' -f 2 | grep -v 'ROOT'`; do
+        echo "      |------ $WEB_APP"
+        echo "      |---------> $($TWIDDLE_CMD_GET "${WEB_APP_MBEAN}${WEB_APP},host=localhost" activeSessions)" | egrep -v "ERROR|at |Exception"
+    done
+
+    echo " "
+
+    #DataSources
+    echo "   Data Sources "
+
+    for DS in `$TWIDDLE_CMD_QRY $JBOSS_JCA_MBEAN | grep ManagedConnectionPool`; do
+        echo "      |--- `echo $DS | cut -d ',' -f 2`"
+        echo "      |---------> $($TWIDDLE_CMD_GET $DS MaxSize)" | egrep -v "ERROR|at |Exception"
+        echo "      |---------> $($TWIDDLE_CMD_GET $DS AvailableConnectionCount)" | egrep -v "ERROR|at |Exception"
+        echo "      |---------> $($TWIDDLE_CMD_GET $DS InUseConnectionCount)" | egrep -v "ERROR|at |Exception"
+        echo "      |---------> Test Connection: $($TWIDDLE_CMD_IVK $DS testConnection)" | egrep -v "ERROR|at |Exception"
+    done
 }
 
-function cleanWorkTmp()
+cleanWorkTmp()
 {
-   # clean tmp and work dirs
-   echo "clean work and tmp dirs from ${JBOSS_PROFILE}..."
-
-   rm -Rf "$JBOSS_HOME/server/${JBOSS_PROFILE}/work"
-   rm -Rf "$JBOSS_HOME/server/${JBOSS_PROFILE}/tmp"
+    # clean tmp and work dirs
+    rm -Rf "$JBOSS_HOME/server/${JBOSS_PROFILE}/work"
+    rm -Rf "$JBOSS_HOME/server/${JBOSS_PROFILE}/tmp"
 }
 
-function jbossPID()
+jbossPID()
 {
-   # try get the JVM PID
-   local jbossPID="x"
-   jbossPID=$(pgrep -f "org\.jboss\.Main -c $JBOSS_PROFILE -b $JBOSS_BINDING_IPADDR -Djboss.service.binding.set=$JBOSS_SERVICE_BIND")
-   echo "$jbossPID"
+    jbossPID=$(pgrep -f "org\.jboss\.Main -c $JBOSS_PROFILE -b $JBOSS_BINDING_IPADDR -Djboss.service.binding.set=$JBOSS_SERVICE_BIND")
+    echo "$jbossPID"
 }
 
 start_profile()
 {
-   cd $JBOSS_HOME/bin
+    # verifica se a instancia jah estah em execucao
+    PID=$(jbossPID)
+    if [ "x$PID" = "x" ]; then
+        echo "starting JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR)..."
+        echo "   using service bind: $JBOSS_BINDING_PORTS"
 
-   # verifica se a instancia jah estah em execucao
-   PID=$(jbossPID)
-   if [ "x$PID" = "x" ]
-   then
-      echo "starting JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR)..."
-      echo "   using service bind: $JBOSS_BINDING_PORTS"
+        #echo "JBOSS_CMD_START=$JBOSS_CMD_START"
 
-      #echo "JBOSS_CMD_START=$JBOSS_CMD_START"
-
-      if [ -z "$SUBIT" ]; then
-         eval $JBOSS_CMD_START >${JBOSS_CONSOLE} 2>&1 &
-      else
-         $SUBIT "$JBOSS_CMD_START >${JBOSS_CONSOLE} 2>&1 &" 
-      fi
-   else
-      echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) is already running [PID $PID]"
-   fi
+        if [ -z "$SU_USER" ]; then
+            eval $JBOSS_CMD_START >${JBOSS_CONSOLE} 2>&1 &
+        else
+            $SU_USER "$JBOSS_CMD_START >${JBOSS_CONSOLE} 2>&1 &" 
+        fi
+    else
+        echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) is already running [PID $PID]"
+    fi
 }
 
 stop_profile()
 {
-   echo "stop JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR)..."
+    echo "stop JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR)..."
 
-   if [ -z "$SUBIT" ]; then
-       $JBOSS_CMD_STOP
-   else
-       #echo "CMD: $JBOSS_CMD_STOP"
-       $SUBIT "$JBOSS_CMD_STOP"
-   fi 
+    if [ -z "$SU_USER" ]; then
+        $JBOSS_CMD_STOP
+    else
+        #echo "CMD: $JBOSS_CMD_STOP"
+        $SU_USER "$JBOSS_CMD_STOP"
+    fi 
 
-   sleep 10
+    sleep 10
 
-   if [ "$CLEAR_WORK_TMP" = "Y" ]; then
-      cleanWorkTmp
-   fi
+    if [ "$CLEAR_WORK_TMP" = "Y" ]; then
+        cleanWorkTmp
+    fi
 }
 
 case "$1" in
@@ -265,60 +240,53 @@ start)
     ;;
 stop)
     stop_profile
-   ;;
+    ;;
 kill)
-   echo "trying halt the JVM process..."
+    echo "trying halt the JVM process..."
 
-   # try get the JVM PID
-   PID=$(jbossPID)
-   if [ "x$PID" = "x" ]
-   then
-      echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) not running! JVM process not found!"
-   else
-      echo "process still running..."
-      echo "killing JBoss (JVM process) [PID $PID]"
-      kill -9 $PID
-   fi
+    PID=$(jbossPID)
+    if [ "x$PID" = "x" ]; then
+        echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) not running! JVM process not found!"
+    else
+        echo "process still running..."
+        echo "killing JBoss (JVM process) [PID $PID]"
+        kill -9 $PID
+    fi
 
-   if [ "$CLEAR_WORK_TMP" = "Y" ]; then
-      cleanWorkTmp
-   fi
-
-   ;;
+    if [ "$CLEAR_WORK_TMP" = "Y" ]; then
+        cleanWorkTmp
+    fi
+    ;;
 restart)
     stop_profile
     start_profile
     ;;
 status)
-   PID=$(jbossPID)
-   if [ "x$PID" = "x" ]; then
-      echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) not running! JVM process not found!"
-      exit 3
-   else
-      echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) is started [PID $PID]"
-      exit 0
-   fi  
-   ;;  
+    PID=$(jbossPID)
+    if [ "x$PID" = "x" ]; then
+        echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) not running! JVM process not found!"
+        exit 3
+    else
+        echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) is started [PID $PID]"
+        exit 0
+    fi  
+    ;;  
 info)
-   clear
+    clear
 
-   # try get the JVM PID
-   PID=$(jbossPID)
-   if [ "x$PID" = "x" ]
-   then
-      echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) not running! JVM process not found!"
-   else
-      echo " "
-      echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) running!"
-      echo "   JBoss (JVM process) [PID $PID] is UP"
-      echo " "
-      echo "   Some server info:"
+    PID=$(jbossPID)
+    if [ "x$PID" = "x" ]; then
+        echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) not running! JVM process not found!"
+    else
+        echo " "
+        echo "JBoss (instance $JBOSS_PROFILE at $JBOSS_BINDING_IPADDR) running!"
+        echo "   JBoss (JVM process) [PID $PID] is UP"
+        echo " "
+        echo "   Some server info:"
 
-      twiddleInfo
-   fi
-   ;;
+        twiddleInfo
+    fi
+    ;;
 *)
-   echo "usage: $0 start|stop|restart|kill|status|info"
+    echo "Usage: $(basename $0) {start|stop|status|restart|kill|info}"
 esac
-
-
