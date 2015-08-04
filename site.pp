d639dbc511b2aafab7ff5cce66840a09fadc47d3
Marcelo F Andrade <mfandrade@gmail.com>
2015-08-04 14:13:21 -0300

    site.pp

--- site.pp
+++ site.pp
@@ -1,6 +1,7 @@
 node /^pje8-jb-(int|ext)-([a-z]).trt8.net$/ {
 
-  $version = '1.6.0'
+  $pje_version = '1.6.0'
+  $environment = 'bugfix'
 
   class { 'pje': version => $pje_version, }
 
@@ -8,16 +9,16 @@ node /^pje8-jb-(int|ext)-([a-z]).trt8.net$/ {
     version         => $pje_version,
     binding_to      => '10.8.14.253',
     jmxremote_port  => '9001',
-    env             => 'producao',
-    ds_databasename => 'pje_1grau_producao',
+    env             => $environment',
+    ds_databasename => "pje_1grau_${environment}",
   }
 
   pje::profile { "${1}${2}2":
     version         => $pje_version,
     binding_to      => '10.8.14.254',
     jmxremote_port  => '9002',
-    env             => 'producao',
-    ds_databasename => 'pje_2grau_producao',
+    env             => $environment,
+    ds_databasename => "pje_2grau_${environment}",
   }
 
 }
